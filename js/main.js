e5885b39acfe06eddcd8c7669decf65acb56294a
Hasnain <mrajhasnain101@gmail.com>
2022-08-25 23:54:47 +0600

    js logic

--- js/main.js
+++ js/main.js
@@ -4,5 +4,14 @@ let seconds=document.getElementById('seconds');
 
 setInterval(()=>{
     let date=new Date();
+    let hr=date.getHours()*30;
+    let min=date.getMinutes()*6;
+    let sec=date.getSeconds()*6;
 
+    hour.style.transform=`rotatez(
+        ${(hr)+(min/12)}deg)`
+    minutes.style.transform=`rotatez(
+        ${min}deg)`
+    seconds.style.transform=`rotatez(
+        ${sec}deg)`
 });
