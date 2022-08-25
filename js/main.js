4349234c91f6340c16504375df94999f890fe986
Hasnain <mrajhasnain101@gmail.com>
2022-08-25 23:38:46 +0600

    js and css

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let hour=document.getElementById('hours');
+let minutes=document.getElementById('minutes');
+let seconds=document.getElementById('seconds');
+
+setInterval(()=>{
+    let date=new Date();
+
+});
